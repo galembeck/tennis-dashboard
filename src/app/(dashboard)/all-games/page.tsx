b702d66fe794714ee913a6d47315a2ed76ea36dd
Pedro Galembeck <galembeckpedro@gmail.com>
2024-09-22 14:37:42 -0300

    refactor: add page-header component

--- src/app/(dashboard)/all-games/page.tsx
+++ src/app/(dashboard)/all-games/page.tsx
@@ -1,6 +1,9 @@
+import { PageHeader } from "@/components/page-header";
+
 export default function AllGamesPage() {
   return (
     <div>
+      <PageHeader />
       <h1>All Games</h1>
     </div>
   );
