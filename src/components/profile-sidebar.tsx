59e141d6ddf372ee936b4ccd1cf9fcca725c6131
Pedro Galembeck <galembeckpedro@gmail.com>
2024-09-23 17:16:42 -0300

    refactor: finished player profile sidebar (card) component

--- src/components/profile-sidebar.tsx
+++ src/components/profile-sidebar.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const ProfileSidebar = () => {
   return (
-    <div className="flex flex-col w-[400px] h-[505px] rounded-[30px] bg-primary-white">
+    <div className="flex flex-col w-[400px] h-[537px] rounded-[30px] bg-primary-white">
       <div className="flex flex-col relative">
         <Image
           src="/assets/profile-sidebar-wave.svg"
@@ -29,11 +30,60 @@ export const ProfileSidebar = () => {
         </div>
       </div>
 
+      <div className="flex flex-col mt-5 px-6">
+        <div className="flex flex-col gap-2">
+          <h1 className="font-semibold text-base">Biography</h1>
+          <Image
+            src="/assets/profile-card-picture.svg"
+            alt="Profile"
+            width={50}
+            height={60}
+          />
+        </div>
+
+        <div className="flex flex-col gap-[10px] mt-[17px] text-sm font-medium">
+          <p>
+            <span className="font-semibold">Age: </span>27
+          </p>
+          <p>
+            <span className="font-semibold">Birth: </span>06 - 07 - 2006
+          </p>
+          <p>
+            <span className="font-semibold">Sex: </span>Men
+          </p>
+          <p>
+            <span className="font-semibold">WTA: </span>10
+          </p>
+        </div>
+
+        <div className="flex flex-col mt-7">
+          <p className="font-semibold text-base">Social Media</p>
+          <div className="flex mt-[10px]">
+            <Link href="/">
+              <Image
+                src="/icons/twitter.svg"
+                alt="Twitter"
+                width={25}
+                height={25}
+              />
+            </Link>
+            <Link href="/">
+              <Image
+                src="/icons/facebook.svg"
+                alt="Facebook"
+                width={25}
+                height={25}
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+
       <Image
         src="/assets/human-player.svg"
         alt="Players"
-        width={277}
-        height={456}
+        width={307}
+        height={486}
         className="flex absolute right-0 mr-8"
       />
     </div>
